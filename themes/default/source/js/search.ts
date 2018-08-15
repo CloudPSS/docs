aad2e7d2c32a88371ebc0143f1cb416d96f61404
OpportunityLiu <opportunity@live.in>
2018-08-15 11:42:53 +0800

    vue
    Former-commit-id: e80ab4840a9241b8170db2403d960003babc337e

--- themes/default/source/js/search.ts
+++ themes/default/source/js/search.ts
@@ -1,3 +1,6 @@
+import Vue from 'vue'
+import { TypeOfExpression } from 'typescript';
+
 (function ()
 {
     interface SearchRecord
@@ -15,84 +18,60 @@
         title: string;
     }
 
+    var vueApp = new Vue({
+        el: '#saerch-form',
+        data:
+        {
+            records: null as (null | Array<FormattedSearchRecord>),
+            term: '',
+            failed: false,
+            succeed: false
+        },
+        computed:
+        {
+            matches: function (): Array<FormattedSearchRecord>
+            {
+                if (!this.records || this.records.length === 0)
+                    return new Array<FormattedSearchRecord>();
+                const terms = this.term.split(/\s+/g).filter(function (s) { return s !== '' });
+                function match(content: string): number
+                {
+                    if (!content)
+                        return 0;
+                    let score = 0;
+                    terms.forEach(term =>
+                    {
+                        if (content.includes(term))
+                            score += (term.length + 1) / content.length;
+                    })
+                    return score;
+                }
+                function getScore(record: FormattedSearchRecord)
+                {
+                    return match(record.formattedTitle) * 10 + match(record.formattedContent);
+                }
+                let records = Array.from<FormattedSearchRecord>(this.records);
+                records.forEach(r => { r.score = getScore(r); });
+                records.sort((a, b) => { return b.score - a.score; });
+                records = records.splice(0, 10).filter(r => { return r.score > 0; });
+                return records;
+            }
+        }
+    })
     const req = new XMLHttpRequest();
-    let succeed = false;
     req.open('get', '/search.json');
     req.onload = function (ev)
     {
         if (this.status == 200)
         {
-            succeed = true;
+            vueApp.succeed = true;
             initSearch(JSON.parse(this.response));
         }
     }
     req.onloadend = function ()
     {
-        if (!succeed)
-            suggest.innerHTML = '<span class="failed"></span>';
+        if (!vueApp.succeed)
+            vueApp.failed = true;
     }
     req.send();
-
-    const queryinput = document.getElementById('search-query') as HTMLInputElement;
-    const suggest = document.getElementById('search-suggest') as HTMLUListElement;
-
-    queryinput.addEventListener('focus', () => { suggest.classList.add('open') });
-    queryinput.addEventListener('blur', () => { setTimeout(() => { suggest.classList.remove('open') }, 50) });
-
-    function initSearch(rawData: ReadonlyArray<SearchRecord>)
-    {
-        let data = rawData.filter(r => { return !!r.title }).map(r =>
-        {
-            const d = r as FormattedSearchRecord;
-            d.formattedContent = r.content.toLowerCase();
-            d.formattedTitle = r.title.toLowerCase();
-            return d;
-        });
-        let currentTerms = null;
-        function refreshCandidates()
-        {
-            const nextTerms = queryinput.value.toLowerCase();
-            if (currentTerms && (currentTerms.trim() === nextTerms.trim()))
-                return;
-            currentTerms = nextTerms;
-
-            const terms = nextTerms.split(/\s+/g).filter(function (s) { return s !== '' });
-            function match(content: string): number
-            {
-                if (!content)
-                    return 0;
-                let score = 0;
-                terms.forEach(term =>
-                {
-                    if (content.includes(term))
-                        score += (term.length + 1) / content.length;
-                })
-                return score;
-            }
-            function getScore(record: FormattedSearchRecord)
-            {
-                return match(record.formattedTitle) * 10 + match(record.formattedContent);
-            }
-            data.forEach(r => { r.score = getScore(r); });
-            data.sort((a, b) => { return b.score - a.score; });
-            while (suggest.firstChild)
-                suggest.removeChild(suggest.firstChild)
-            for (const record of data)
-            {
-                if (suggest.childNodes.length >= 8)
-                    break;
-                if (record.score === 0)
-                    break;
-                const htmlCandidate = document.createElement('li');
-                htmlCandidate.innerHTML = (record.title || record.content.substr(0, 20).trim()).link(record.url);
-                suggest.appendChild(htmlCandidate);
-            }
-            if (!suggest.firstChild)
-                suggest.innerHTML = '<span class="no-result"></span>';
-        }
-        refreshCandidates();
-
-        queryinput.addEventListener('input', refreshCandidates);
-        queryinput.addEventListener('focus', refreshCandidates);
-    }
 })();
