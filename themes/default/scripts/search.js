6005461e49a49256c2f55e70fc61d2426ca1bbfe
OpportunityLiu <opportunity@live.in>
2018-08-20 15:13:59 +0800

    saerch
    Former-commit-id: 6147c8ae4060d3292412c72455fb3e5efc29f7ba

--- themes/default/scripts/search.js
+++ themes/default/scripts/search.js
@@ -1,28 +1,38 @@
 var pathFn = require('path');
 var fs = require('fs');
+var pinyin = require('pinyinjs');
 
 hexo.extend.generator.register('search', searchGenerator);
 
-function searchGenerator(locals) {
+function searchGenerator(locals)
+{
     const pages = locals.pages;
-    
-    var res = new Array() 
-    pages.each(function (page) { 
-        var temp_page = new Object() 
-        if (page.title) { 
-            temp_page.title = page.title.trim();
-        } 
-        if (page.path) { 
+
+    var res = new Array()
+    pages.each(function (page)
+    {
+        var temp_page = new Object()
+        temp_page.title = hexo.extend.helper.get('title')(page);
+        temp_page.extend = [
+            pinyin(temp_page.title, { style: pinyin.STYLE_FIRST_LETTER }).join(''),
+            pinyin(temp_page.title, { style: pinyin.STYLE_INITIALS }).join(''),
+            pinyin(temp_page.title, { style: pinyin.STYLE_NORMAL }).join('')
+        ].join(' ');
+
+        if (page.path)
+        {
             temp_page.url = hexo.config.root + page.path;
-        } 
-        if (page._content) { 
+        }
+        if (page._content)
+        {
             temp_page.content = page._content.trim();
         }
-        if (page.categories && page.categories.length > 0) {
+        if (page.categories && page.categories.length > 0)
+        {
             temp_page.categories = Array.from(page.categories);
-        } 
-        res.push(temp_page);  
-    }); 
+        }
+        res.push(temp_page);
+    });
 
     var json = JSON.stringify(res);
 
