8bcf416c92e47af44fc49c15a08a9f51fbb6a6a9
OpportunityLiu <opportunity@live.in>
2018-12-16 19:31:36 +0800

    Revert ".."
    This reverts commit dff45d382299f3952532d9a15f91df48109baab3 [formerly 5d1c4bf918768c0224e5b08050b3d5b8b496a4e7].
    
    
    Former-commit-id: 82ea37e29d669876138bb6e2f55d13a5d2ea0b96

--- themes/default/source/js/mxgraph-loader.ts
+++ themes/default/source/js/mxgraph-loader.ts
@@ -3,60 +3,60 @@ import "mxGraph/Load";
 
 import Util from './util';
 
-async function loadMxGraph(container: Element, padding?: number)
-{
-    padding = padding || 12;
-    const getshape = Util.fetchShape(<string>container.getAttribute('symbol'));
-
-    // Disables the built-in context menu
-    mx.mxEvent.disableContextMenu(container);
-    const editor = new mx.Editor();
-    // Creates the graph inside the given container
-    const graph = new mx.Graph(container, null, null, editor.graph.getStylesheet());
-    graph.resetViewOnRootChange = false;
-    graph.foldingEnabled = false;
-    graph.gridEnabled = false;
-    graph.autoScroll = false;
-    graph.setTooltips(false);
-    graph.setConnectable(false);
-    graph.setEnabled(false);
-    // Sets the base style for all vertices
-    const style = graph.getStylesheet().getDefaultVertexStyle();
-    style[mx.mxConstants.STYLE_ROUNDED] = true;
-    style[mx.mxConstants.STYLE_FILLCOLOR] = '#ffffff';
-    style[mx.mxConstants.STYLE_STROKECOLOR] = '#000000';
-    style[mx.mxConstants.STYLE_STROKEWIDTH] = '2';
-    style[mx.mxConstants.STYLE_FONTCOLOR] = '#000000';
-    style[mx.mxConstants.STYLE_FONTSIZE] = '12';
-    style[mx.mxConstants.STYLE_FONTSTYLE] = 1;
-    graph.getStylesheet().putDefaultVertexStyle(style);
-
-    const shape = (await getshape).shape;
-    const firstChild = <Element>shape.firstChild;
-    mx.mxStencilRegistry.parseStencilSet(firstChild);
-    const parent = graph.getDefaultParent();
-    const width = Number(firstChild.getAttribute('w'));
-    const height = Number(firstChild.getAttribute('h'));
-
-    graph.getModel().beginUpdate();
-    try
+    async function loadMxGraph(container: Element, padding?: number)
     {
-        graph.insertVertex(parent, null, '', 0, 0, width, height, 'shape=' + (firstChild.getAttribute('name') || '').toLowerCase());
-    }
-    finally
-    {
-        graph.getModel().endUpdate();
-    }
-    const svg = container.getElementsByTagName("svg")[0];
-    svg.viewBox.baseVal.width = width + padding * 2;
-    svg.viewBox.baseVal.height = height + padding * 2;
-    svg.viewBox.baseVal.x = -padding;
-    svg.viewBox.baseVal.y = -padding;
-    svg.style.minWidth = svg.viewBox.baseVal.width + 'px';
-    svg.style.minHeight = svg.viewBox.baseVal.height + 'px';
-    svg.style.maxWidth = svg.viewBox.baseVal.width * 2 + 'px';
-    svg.style.maxHeight = svg.viewBox.baseVal.height * 2 + 'px';
-};
+        padding = padding || 12;
+        const getshape = Util.fetchShape(<string>container.getAttribute('symbol'));
 
-for (const container of Array.from(document.getElementsByTagName("mx-graph")))
-    loadMxGraph(container);
+        // Disables the built-in context menu
+        mx.mxEvent.disableContextMenu(container);
+        const editor = new mx.Editor();
+        // Creates the graph inside the given container
+        const graph = new mx.Graph(container, null, null, editor.graph.getStylesheet());
+        graph.resetViewOnRootChange = false;
+        graph.foldingEnabled = false;
+        graph.gridEnabled = false;
+        graph.autoScroll = false;
+        graph.setTooltips(false);
+        graph.setConnectable(false);
+        graph.setEnabled(false);
+        // Sets the base style for all vertices
+        const style = graph.getStylesheet().getDefaultVertexStyle();
+        style[mx.mxConstants.STYLE_ROUNDED] = true;
+        style[mx.mxConstants.STYLE_FILLCOLOR] = '#ffffff';
+        style[mx.mxConstants.STYLE_STROKECOLOR] = '#000000';
+        style[mx.mxConstants.STYLE_STROKEWIDTH] = '2';
+        style[mx.mxConstants.STYLE_FONTCOLOR] = '#000000';
+        style[mx.mxConstants.STYLE_FONTSIZE] = '12';
+        style[mx.mxConstants.STYLE_FONTSTYLE] = 1;
+        graph.getStylesheet().putDefaultVertexStyle(style);
+
+        const shape = (await getshape).shape;
+        const firstChild = <Element>shape.firstChild;
+        mx.mxStencilRegistry.parseStencilSet(firstChild);
+        const parent = graph.getDefaultParent();
+        const width = Number(firstChild.getAttribute('w'));
+        const height = Number(firstChild.getAttribute('h'));
+
+        graph.getModel().beginUpdate();
+        try
+        {
+            graph.insertVertex(parent, null, '', 0, 0, width, height, 'shape=' + (firstChild.getAttribute('name') || '').toLowerCase());
+        }
+        finally
+        {
+            graph.getModel().endUpdate();
+        }
+        const svg = container.getElementsByTagName("svg")[0];
+        svg.viewBox.baseVal.width = width + padding * 2;
+        svg.viewBox.baseVal.height = height + padding * 2;
+        svg.viewBox.baseVal.x = -padding;
+        svg.viewBox.baseVal.y = -padding;
+        svg.style.minWidth = svg.viewBox.baseVal.width + 'px';
+        svg.style.minHeight = svg.viewBox.baseVal.height + 'px';
+        svg.style.maxWidth = svg.viewBox.baseVal.width * 2 + 'px';
+        svg.style.maxHeight = svg.viewBox.baseVal.height * 2 + 'px';
+    };
+
+    for (const container of Array.from(document.getElementsByTagName("mx-graph")))
+        loadMxGraph(container);
