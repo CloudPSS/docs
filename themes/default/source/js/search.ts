3033368f63617789239cbbb84ee4dfca04c53870
OpportunityLiu <opportunity@live.in>
2018-12-16 17:25:57 +0800

    try
    Former-commit-id: ad8b0086749ea7a838e589c6b62d3f90a779f0b1

--- themes/default/source/js/search.ts
+++ themes/default/source/js/search.ts
@@ -1,163 +1,159 @@
 import Vue from 'vue';
+import Util from './util';
 
-interface SearchRecord
+namespace Search
 {
-    url?: string;
-    content?: string;
-    title?: string;
-    type?: string;
-    category?: number;
-
-    extend?: string;
-}
-
-interface SiteMap
-{
-    [key: string]: Type;
-}
-
-interface Type
-{
-    name: string;
-    categories?: CategoryMap;
-}
-
-interface CategoryMap
-{
-    [key: number]: string[] | undefined;
-}
-
-class FormattedSearchRecord
-{
-    url: string;
-    formattedUrl: string;
-    content: string;
-    categories: string[];
-    score = 0;
-    formattedTitle: string;
-    formattedContent: string;
-    title: string;
-    extend: string;
-    type?: string;
-
-    constructor(record: SearchRecord, sitemap: SiteMap)
+    interface SearchRecord
     {
-        this.url = record.url || '';
-        this.formattedUrl = this.url.toLowerCase();
-        this.type = record.type || undefined;
-        this.content = (record.content || '').trim();
-        this.formattedContent = this.content.toLowerCase();
-        this.title = (record.title || '').trim();
-        this.formattedTitle = this.title.toLowerCase();
-        this.categories = [];
-        if (record.category && record.type)
-        {
-            let cat = sitemap[record.type].categories;
-            if (cat)
-                this.categories = cat[record.category] || [];
-        }
-        this.extend = record.extend || '';
+        url?: string;
+        content?: string;
+        title?: string;
+        type?: string;
+        category?: number;
+
+        extend?: string;
     }
-}
 
-const queryinput = document.getElementById('search-query') as HTMLInputElement;
-const suggest = document.getElementById('search-suggest') as HTMLUListElement;
-queryinput.addEventListener('focus', () => { suggest.classList.add('open') });
-queryinput.addEventListener('blur', () => { setTimeout(() => { suggest.classList.remove('open') }, 50) });
+    interface SiteMap
+    {
+        [key: string]: Type;
+    }
 
-const vueApp = new Vue({
-    el: '#search-form',
-    data:
+    interface Type
     {
-        records: null as (null | Array<FormattedSearchRecord>),
-        term: '',
-        failed: false,
-        loadedPercent: 0,
-        suggestOpen: false,
-        sitemap: null as (null | SiteMap)
-    },
-    methods:
+        name: string;
+        categories?: CategoryMap;
+    }
+
+    interface CategoryMap
     {
-        showSuggest()
+        [key: number]: string[] | undefined;
+    }
+
+    class FormattedSearchRecord
+    {
+        url: string;
+        formattedUrl: string;
+        content: string;
+        categories: string[];
+        score = 0;
+        formattedTitle: string;
+        formattedContent: string;
+        title: string;
+        extend: string;
+        type?: string;
+
+        constructor(record: SearchRecord, sitemap: SiteMap)
         {
-            this.suggestOpen = true;
-        },
-        hideSuggest()
-        {
-            setTimeout(() =>
+            this.url = record.url || '';
+            this.formattedUrl = this.url.toLowerCase();
+            this.type = record.type || undefined;
+            this.content = (record.content || '').trim();
+            this.formattedContent = this.content.toLowerCase();
+            this.title = (record.title || '').trim();
+            this.formattedTitle = this.title.toLowerCase();
+            this.categories = [];
+            if (record.category && record.type)
             {
-                this.suggestOpen = false;
-            }, 100);
-        },
-        submit()
-        {
-            let match = this.matches[0];
-            if (!match || !this.term.trim())
-                return;
-            if (match.title === this.term.trim() || this.matches.length === 1)
-                window.location.assign(match.url);
+                let cat = sitemap[record.type].categories;
+                if (cat)
+                    this.categories = cat[record.category] || [];
+            }
+            this.extend = record.extend || '';
         }
-    },
-    computed:
-    {
-        succeed: function ()
+    }
+
+    const queryinput = document.getElementById('search-query') as HTMLInputElement;
+    const suggest = document.getElementById('search-suggest') as HTMLUListElement;
+    queryinput.addEventListener('focus', () => { suggest.classList.add('open') });
+    queryinput.addEventListener('blur', () => { setTimeout(() => { suggest.classList.remove('open') }, 50) });
+
+    const vueApp = new Vue({
+        el: '#search-form',
+        data:
         {
-            return !!(this.sitemap && this.records && !this.failed);
+            records: null as (null | Array<FormattedSearchRecord>),
+            term: '',
+            failed: false,
+            loadedPercent: 0,
+            suggestOpen: false,
+            sitemap: null as (null | SiteMap)
         },
-        matches: function (): Array<FormattedSearchRecord>
+        methods:
         {
-            if (!this.records || this.records.length === 0)
-                return new Array<FormattedSearchRecord>();
-            const terms = this.term.toLowerCase().split(/\s+/g).filter(function (s) { return s !== '' });
-            function match(content: string): number
+            showSuggest()
             {
-                if (!content)
-                    return 0;
-                let score = 0;
-                terms.forEach(term =>
+                this.suggestOpen = true;
+            },
+            hideSuggest()
+            {
+                setTimeout(() =>
                 {
-                    if (content.indexOf(term) >= 0)
-                        score += (term.length + 1) / content.length;
-                })
-                return score;
-            }
-            function getScore(record: FormattedSearchRecord)
+                    this.suggestOpen = false;
+                }, 100);
+            },
+            submit()
             {
-                return match(record.formattedTitle) * 10 + match(record.extend) * 5 + match(record.formattedUrl) * 5
-                    + match(record.formattedContent)
-                    + record.categories.reduce((sum, cat) => sum + match(cat), 0);
+                let match = this.matches[0];
+                if (!match || !this.term.trim())
+                    return;
+                if (match.title === this.term.trim() || this.matches.length === 1)
+                    window.location.assign(match.url);
             }
-            let records = this.records.slice();
-            records.forEach(r => { r.score = getScore(r); });
-            records.sort((a, b) => { return b.score - a.score; });
-            records = records.splice(0, 10).filter(r => { return r.score > 0; });
-            return records;
+        },
+        computed:
+        {
+            succeed: function ()
+            {
+                return !!(this.sitemap && this.records && !this.failed);
+            },
+            matches: function (): Array<FormattedSearchRecord>
+            {
+                if (!this.records || this.records.length === 0)
+                    return new Array<FormattedSearchRecord>();
+                const terms = this.term.toLowerCase().split(/\s+/g).filter(function (s) { return s !== '' });
+                function match(content: string): number
+                {
+                    if (!content)
+                        return 0;
+                    let score = 0;
+                    terms.forEach(term =>
+                    {
+                        if (content.indexOf(term) >= 0)
+                            score += (term.length + 1) / content.length;
+                    })
+                    return score;
+                }
+                function getScore(record: FormattedSearchRecord)
+                {
+                    return match(record.formattedTitle) * 10 + match(record.extend) * 5 + match(record.formattedUrl) * 5
+                        + match(record.formattedContent)
+                        + record.categories.reduce((sum, cat) => sum + match(cat), 0);
+                }
+                let records = this.records.slice();
+                records.forEach(r => { r.score = getScore(r); });
+                records.sort((a, b) => { return b.score - a.score; });
+                records = records.splice(0, 10).filter(r => { return r.score > 0; });
+                return records;
+            }
         }
-    }
-})
+    });
 
-async function getJson(path: string): Promise<any>
-{
-    const response = await fetch(path);
-    if (!response.ok)
-        throw new Error(response.statusText);
-    return await response.json();
+    (async function ()
+    {
+        try
+        {
+            const getSearch = Util.fetchJson<SearchRecord[]>('/search.json');
+            const getSitemap = Util.fetchJson<SiteMap>('/sitemap.json');
+            const data = await getSearch;
+            const sitemap = await getSitemap;
+            vueApp.loadedPercent = 100;
+            vueApp.sitemap = sitemap;
+            vueApp.records = data.filter(r => r.title).map(r => new FormattedSearchRecord(r, sitemap));
+        }
+        catch
+        {
+            vueApp.failed = true;
+        }
+    })();
 }
-
-(async function ()
-{
-    try
-    {
-        let getSearch = getJson('/search.json');
-        let getSitemap = getJson('/sitemap.json');
-        let data = await getSearch as SearchRecord[];
-        const sitemap = await getSitemap;
-        vueApp.loadedPercent = 100;
-        vueApp.sitemap = sitemap;
-        vueApp.records = data.filter(r => r.title).map(r => new FormattedSearchRecord(r, sitemap));
-    }
-    catch
-    {
-        vueApp.failed = true;
-    }
-})();
---
+++ themes/default/source/js/util.ts
@@ -0,0 +1,120 @@
+﻿namespace Util
+{
+    export const DOMContentLoaded: () => Promise<void> = (function ()
+    {
+        let isReady = false;
+        let waiting = new Array<() => void>();
+        document.addEventListener('DOMContentLoaded', function ()
+        {
+            isReady = true;
+            for (const cb of waiting)
+                cb();
+            waiting = [];
+        }, false);
+
+        return function DOMContentLoaded(): Promise<void>
+        {
+            if (isReady)
+                return Promise.resolve();
+            return new Promise(function (resolve)
+            {
+                waiting.push(resolve);
+            });
+        }
+    })();
+
+    export async function fetchJson<T>(url: string | URL): Promise<T>
+    {
+        const urlData = (typeof url === 'string') ? new URL(url) : url;
+        const response = await fetch(urlData.href);
+        if (!response.ok)
+            throw new Error(response.statusText);
+        return await response.json() as T;
+    }
+
+    export function fetchJsonP<T>(url: string | URL): Promise<T>
+    {
+        const urlData = (typeof url === 'string') ? new URL(url) : url;
+        urlData.searchParams.set('callback', 'resolve');
+        return new Promise(async function (resolve, reject)
+        {
+            const response = await fetch(urlData.href);
+            eval(await response.text());
+        })
+    }
+
+    interface ShapeData
+    {
+        classname: string;
+        shape: Document;
+    }
+
+    export async function fetchShape(symbol: string): Promise<ShapeData>
+    {
+        interface ShapeResponse
+        {
+            classname: string;
+            shape: string;
+        }
+        const data = await fetchJsonP<ShapeResponse>(`http://192.168.0.133/editor/getCompShapeBySym/?sym=${symbol}`);
+        return {
+            classname: data.classname,
+            shape: mxUtils.parseXml(data.shape)
+        };
+    }
+
+    export interface CompModel
+    {
+
+    }
+
+    export function fetchCompModel(classname: string): Promise<CompModel>
+    {
+        return fetchJsonP(`http://192.168.0.133/editor/getCompModelByClassname/?classname=${classname}`);
+    }
+
+    // Stolen from: https://github.com/hexojs/hexo-util/blob/master/lib/escape_regexp.js
+    export function escapeRegExp(str: string)
+    {
+        // http://stackoverflow.com/a/6969486
+        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    }
+
+    // Stolen from: https://github.com/hexojs/hexo-util/blob/master/lib/slugize.js
+    export function slugize(str: string, options?: { separator?: string; transform?: ((input: string) => string); })
+    {
+        const option = options || {}
+
+        const rControl = /[\u0000-\u001f]/g
+        const rSpecial = /[\s~`!@#\$%\^&\*\(\)\-_\+=\[\]\{\}\|\\;:"'<>,\.\?\/]+/g
+        const separator = option.separator || '-'
+        const escapedSep = escapeRegExp(separator)
+
+        var result = str
+            // Remove control characters
+            .replace(rControl, '')
+            // Replace special characters
+            .replace(rSpecial, separator)
+            // Remove continuous separators
+            .replace(new RegExp(escapedSep + '{2,}', 'g'), separator)
+            // Remove prefixing and trailing separators
+            .replace(new RegExp('^' + escapedSep + '+|' + escapedSep + '+$', 'g'), '')
+
+        if (options && options.transform)
+            return options.transform(result);
+        else
+            return result;
+    }
+
+    export function htmlEscape(text: string)
+    {
+        return text
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+    }
+}
+
+export default Util;
