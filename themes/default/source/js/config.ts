0512958e951b2cf9fb7be9556fa8b4e4123110f0
OpportunityLiu <opportunity@live.in>
2018-12-16 19:31:50 +0800

    Revert "??"
    This reverts commit 2d88b1200424cf7de8ec544c3f5da05b499067ab.

--- themes/default/source/js/config.ts
+++ themes/default/source/js/config.ts
@@ -1,7 +1,5 @@
-declare const IS_DEBUGGING: boolean;
-
 require.config({
     paths: {
-        vue: IS_DEBUGGING ? '//cdn.jsdelivr.net/npm/vue@2/dist/vue' : '//cdn.jsdelivr.net/npm/vue@2/dist/vue.min',
+        vue: IS_DEBUGGING ? '//cdn.jsdelivr.net/npm/vue@2/dist/vue' : '//cdn.jsdelivr.net/npm/vue@2/dist/vue',
     }
 });
