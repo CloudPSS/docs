e8b0a87b6b4a5d7982958beeff47860883376580
OpportunityLiu <opportunity@live.in>
2018-12-16 19:31:47 +0800

    Revert "try"
    This reverts commit ad8b0086749ea7a838e589c6b62d3f90a779f0b1.

--- themes/default/source/js/footer.ts
+++ themes/default/source/js/footer.ts
@@ -1,33 +1,28 @@
-namespace Footer
+(function ()
 {
-    function init()
+    const link = document.querySelector("footer.page-footer .author[data]") as HTMLSpanElement;
+    if (link)
     {
-        const link = document.querySelector("footer.page-footer .author[data]") as HTMLSpanElement;
-        if (link)
+        const data = (link.getAttribute('data') || '').trim();
+        link.removeAttribute('data');
+        if(!data)
+            return;
+        const mail = atob(atob(atob(data)));
+        const mailspan = document.createElement('span');
+        mailspan.innerText = `(${mail})`;
+        mailspan.classList.add('print-only');
+        link.addEventListener('click', () =>
         {
-            const data = (link.getAttribute('data') || '').trim();
-            link.removeAttribute('data');
-            if (!data)
-                return;
-            const mail = atob(atob(atob(data)));
-            const mailspan = document.createElement('span');
-            mailspan.innerText = `(${mail})`;
-            mailspan.classList.add('print-only');
-            link.addEventListener('click', () =>
-            {
-                const win = window.open('mailto:' + mail, 'emailWindow');
-                setTimeout(() => { if (win && win.open && !win.closed) win.close(); }, 0);
-            });
-            window.addEventListener('beforeprint', () =>
-            {
-                link.appendChild(mailspan);
-            });
-            window.addEventListener('afterprint', () =>
-            {
-                link.removeChild(mailspan);
-            });
-        }
+            const win = window.open('mailto:' + mail, 'emailWindow');
+            setTimeout(() => { if (win && win.open && !win.closed) win.close(); }, 0);
+        });
+        window.addEventListener('beforeprint', () =>
+        {
+            link.appendChild(mailspan);
+        });
+        window.addEventListener('afterprint', () =>
+        {
+            link.removeChild(mailspan);
+        });
     }
-
-    init();
-}
+})();
