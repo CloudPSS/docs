ad8b0086749ea7a838e589c6b62d3f90a779f0b1
OpportunityLiu <opportunity@live.in>
2018-12-16 17:25:57 +0800

    try

--- themes/default/source/js/config.ts
+++ themes/default/source/js/config.ts
@@ -1,7 +1,13 @@
 declare const IS_DEBUGGING: boolean;
+declare const PAGE_TYPE: string;
+declare const IS_INDEX: boolean;
 
 require.config({
     paths: {
         vue: IS_DEBUGGING ? '//cdn.jsdelivr.net/npm/vue@2/dist/vue' : '//cdn.jsdelivr.net/npm/vue@2/dist/vue.min',
+        chart: '//cdn.jsdelivr.net/npm/chart.js@2/dist/Chart.min',
+        mermaid: '//cdn.jsdelivr.net/npm/mermaid@8.0.0-rc.8/dist/mermaid.min',
+        fastclick: '//cdn.jsdelivr.net/npm/fastclick@1/lib/fastclick.min',
+        mxClient: '//cdn.jsdelivr.net/npm/mxgraph@3.7.1/javascript/mxClient.min'
     }
 });
--- themes/default/source/js/footer.ts
+++ themes/default/source/js/footer.ts
@@ -1,28 +1,33 @@
-(function ()
+namespace Footer
 {
-    const link = document.querySelector("footer.page-footer .author[data]") as HTMLSpanElement;
-    if (link)
+    function init()
     {
-        const data = (link.getAttribute('data') || '').trim();
-        link.removeAttribute('data');
-        if(!data)
-            return;
-        const mail = atob(atob(atob(data)));
-        const mailspan = document.createElement('span');
-        mailspan.innerText = `(${mail})`;
-        mailspan.classList.add('print-only');
-        link.addEventListener('click', () =>
+        const link = document.querySelector("footer.page-footer .author[data]") as HTMLSpanElement;
+        if (link)
         {
-            const win = window.open('mailto:' + mail, 'emailWindow');
-            setTimeout(() => { if (win && win.open && !win.closed) win.close(); }, 0);
-        });
-        window.addEventListener('beforeprint', () =>
-        {
-            link.appendChild(mailspan);
-        });
-        window.addEventListener('afterprint', () =>
-        {
-            link.removeChild(mailspan);
-        });
+            const data = (link.getAttribute('data') || '').trim();
+            link.removeAttribute('data');
+            if (!data)
+                return;
+            const mail = atob(atob(atob(data)));
+            const mailspan = document.createElement('span');
+            mailspan.innerText = `(${mail})`;
+            mailspan.classList.add('print-only');
+            link.addEventListener('click', () =>
+            {
+                const win = window.open('mailto:' + mail, 'emailWindow');
+                setTimeout(() => { if (win && win.open && !win.closed) win.close(); }, 0);
+            });
+            window.addEventListener('beforeprint', () =>
+            {
+                link.appendChild(mailspan);
+            });
+            window.addEventListener('afterprint', () =>
+            {
+                link.removeChild(mailspan);
+            });
+        }
     }
-})();
+
+    init();
+}
