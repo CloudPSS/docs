82f1600fd30a29b08a50f68725486a9b44ff4629
Opportunity <opportunity@live.in>
2020-11-18 21:48:26 +0800

    build: update script
    Former-commit-id: 46365795de82918d06d266ba08ab62fef9af4c14

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,7 @@ export const environment = {
  */
 export function docUrls(ref: string): string[] {
     return [
+        `${document.baseURI}/content/`,
         `https://cdn.jsdelivr.net/gh/CloudPSS/docs@${ref}/docs/`,
         `https://gitcdn.xyz/cdn/CloudPSS/docs/${ref}/docs/`,
         `https://rawcdn.githack.com/CloudPSS/docs/${ref}/docs/`,
