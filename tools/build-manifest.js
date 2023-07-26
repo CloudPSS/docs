e81c2b4365f3a58dadfe18fab74302d5225c789d
OpportunityLiu <opportunity@live.in>
2023-07-26 15:30:29 +0800

    fix: tool
    Former-commit-id: 344c21503a40e1a0d711072851ffa47a6379bd48

--- tools/build-manifest.js
+++ tools/build-manifest.js
@@ -1,45 +1,56 @@
+import p from 'node:path';
+import { fileURLToPath } from 'node:url';
+import markdownIt from 'markdown-it';
+import markdownItFrontMatter from 'markdown-it-front-matter';
 import yaml from 'js-yaml';
 import fs from 'fs-extra';
 import { glob } from 'glob';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'node:path/posix';
 
-import markdownIt from 'markdown-it';
-import markdownItFrontMatter from 'markdown-it-front-matter';
+process.chdir(p.resolve(p.dirname(fileURLToPath(import.meta.url)), '../docs'));
 
-process.chdir(path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../docs'));
+let fm;
+const md = markdownIt({
+    html: true,
+    typographer: true,
+}).use(markdownItFrontMatter, (f) => (fm = yaml.load(f)));
+
+/**
+ * Markdown to HTML
+ * @param {string} data Markdown
+ * @returns {{fm: object, content: string}} Front matter and HTML
+ */
+function render(data) {
+    fm = undefined;
+    const content = md.render(data);
+    return {
+        fm: fm && typeof fm == 'object' ? fm : {},
+        content,
+    };
+}
 
 try {
-    let fm;
-    const md = markdownIt({
-        html: true,
-        typographer: true,
-    }).use(markdownItFrontMatter, (f) => (fm = f));
-
     const base = /** @type {import('../src/app/interfaces/manifest.js').Manifest} */ (
         yaml.load(await fs.readFile('./manifest.yml', 'utf8'))
     );
-    const matches = await glob('**/*.md');
+    const matches = await glob('**/*.md', { posix: true });
     matches.sort();
     base.documents = {};
     const info = await Promise.all(
         matches.map(async (v) => {
             const file = await fs.readFile(v, 'utf8');
-            fm = undefined;
-            md.render(file);
-            const front = fm ? yaml.load(fm) : undefined;
-            const frontObj = typeof front == 'object' ? front : undefined;
-
+            const { fm } = render(file);
             const filename = path.basename(v, '.md');
             let title = filename;
             if (filename.toLowerCase() === 'index') {
                 title = path.basename(path.dirname(v));
             }
 
-            return [`/${v}`, { title, ...frontObj, content: file }];
+            return /** @type {const} */ ([`/${v}`, { title, ...fm, content: file }]);
         }),
     );
-    Object.assign(base.documents, Object.fromEntries(info));
+    const map = Object.fromEntries(info);
+    Object.assign(base.documents, map);
     await fs.writeFile('./manifest.json', JSON.stringify(base), 'utf8');
 } catch (ex) {
     // eslint-disable-next-line no-console
