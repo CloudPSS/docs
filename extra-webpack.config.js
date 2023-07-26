d031f0290ad5e97dba2427f235d38fa138a25284
OpportunityLiu <opportunity@live.in>
2023-07-26 15:13:13 +0800

    refactor: use esm for tools

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,14 +1,11 @@
-const webpack = require('webpack');
-const _ = require('lodash');
-
 /**
  *
  * @param {import('webpack').Configuration} config
  */
-module.exports = function (config) {
+export default function (config) {
     config.module.rules.push({
         test: /\.ya?ml$/,
         use: 'yaml-loader',
     });
     return config;
-};
+}
