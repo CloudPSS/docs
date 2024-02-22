99640185214bcd61fcf6848855c29ba7d478d398
opportunity <opportunity@live.in>
2024-02-22 08:10:41 +0000

    feat: i18n utils
    Former-commit-id: 5d4cb67fa915bc3dcdf09b53e489ab9b861a6182

--- docusaurus/i18n.ts
+++ docusaurus/i18n.ts
@@ -1,12 +1,21 @@
 import type { Config } from '@docusaurus/types';
 
+/** 进行本地化的语言 */
+const locales = [
+    'zh-hans',
+    // 'zh-hant',
+    // 'en',
+] as const;
+
+/** 默认语言 */
+const defaultLocale: Locale = 'zh-hans';
+
 const config = {
-    defaultLocale: 'zh-hans',
-    locales: [
-        'zh-hans',
-        // 'zh-hant',
-        // 'en',
-    ],
+    defaultLocale,
+    locales: [...locales],
 } satisfies Config['i18n'];
 
 export default config;
+
+/** 进行本地化的语言 */
+export type Locale = (typeof locales)[number];
