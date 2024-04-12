f3aa0fc8e1ee08b61ca8420e89a6af86e9bb81a2
OpportunityLiu <opportunity@live.in>
2024-04-12 12:44:03 +0800

    feat: rename to kb

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
     favicon: 'icons/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://docs.cloudpss.net',
+    url: 'https://kb.cloudpss.net',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: BASE_URL,
