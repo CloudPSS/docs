27f8340f5e706942e61f62aef90ae331c51f9be3
OpportunityLiu <opportunity@live.in>
2024-08-28 14:19:25 +0800

    build: update deps

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 import config from '@cloudpss/lint-staged-config';
 
-export default /** @type {import('lint-staged').Config} */ ({
+export default /** @type {import('@cloudpss/lint-staged-config').Config} */ ({
   ...config,
   '*.zip': 'node ./tools/optimize-zip.js',
   '*.{png,jpg,jpeg,gif}': 'node ./tools/convert-webp.js',
