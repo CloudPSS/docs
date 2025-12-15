dd4b05f444ece00bc6a6d9ce6b85ad4e50fe1463
yh <1094779545@qq.com>
2025-12-15 04:08:24 +0000

    docs: 修复提交报错

--- docusaurus/navbars.ts
+++ docusaurus/navbars.ts
@@ -53,7 +53,7 @@ const navbars: NonNullable<ThemeConfig['navbar']>['items'] = [
         position: 'left',
         label: '案例',
         type: 'docSidebar',
-        sidebarId: 'cases-old',
+        sidebarId: 'cases_old',
     },
     {
         position: 'left',
