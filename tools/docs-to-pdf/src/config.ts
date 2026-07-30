40a64e23754a7f4e288a3c21fa02b9539b7b2131
OpportunityLiu <opportunity@live.in>
2026-07-30 17:31:34 +0800

    build: update deps

--- tools/docs-to-pdf/src/config.ts
+++ tools/docs-to-pdf/src/config.ts
@@ -14,19 +14,18 @@ export const HOST_REPLACE = 'https://kb.cloudpss.net';
  * 生成文档的范围
  */
 export const RANGES: Range[] = [
-    ['https://kb.cloudpss.net/documents/quick-start/quick-start/'],
-    ['https://kb.cloudpss.net/documents/software/'],
+    [
+        'https://kb.cloudpss.net/documents/software/xstudio/',
+        'https://kb.cloudpss.net/documents/software/emtlab/emtlab-sdk/',
+    ],
+    [
+        'https://kb.cloudpss.net/documents/software/user-center/',
+        'https://kb.cloudpss.net/documents/software/sdk-python/api/dslab/',
+    ],
     ['https://kb.cloudpss.net/documents/hardware/desktop-type/'],
-    ['https://kb.cloudpss.net/documents/software-tools/conversion-tools/'],
     ['https://kb.cloudpss.net/documents/maintenance-tools/server-manager/'],
+    [
+        'https://kb.cloudpss.net/documents/software-tools/conversion-tools/',
+        'https://kb.cloudpss.net/documents/software-tools/signal-monitor/',
+    ],
 ];
-// export const RANGES: Range[] = [['/documents/software/emtlab/', '/documents/software/emtlab/introduction-to-emtlab/']];
-// export const RANGES: Range[] = [
-//     ['/documents/software/emtlab/', '/documents/software/emtlab/component-library/hardware-in-loop/VirtualOutput/'],
-// ];
-// export const RANGES: Range[] = [
-//     [
-//         '/documents/software/emtlab/parallel-acceleration/advanced-param-setup/',
-//         '/documents/software/emtlab/parallel-acceleration/advanced-param-setup/',
-//     ],
-// ];
